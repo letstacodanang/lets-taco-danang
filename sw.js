5135e657a3c84bac46a7afb3362013a56f2d53d5
letstacodanang <letstacodanang@gmail.com>
2026-09-20 04:15:25 +0700

    Admin: fix Mark Paid and other buttons calling undefined functions; use signed-in login for writes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'letstaco-v4';
+var CACHE = 'letstaco-v5';
 var ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(cache) {
