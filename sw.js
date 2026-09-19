4d3086fa7d493a2dc0699d7fb00cf24ffcd94caf
letstacodanang <letstacodanang@gmail.com>
2026-09-20 03:05:30 +0700

    Pricing: menu-accurate combos (mixed flavors, priciest sets price, min 2 tacos), price x qty for singles, updated chips/soda/salsa prices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'letstaco-v3';
+var CACHE = 'letstaco-v4';
 var ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(cache) {
