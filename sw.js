a680f366f88c7f6e4f6c91a50465b80eeed10cfd
letstacodanang <letstacodanang@gmail.com>
2026-09-20 09:27:01 +0700

    Calm refresh: no redraw when orders unchanged, keep open cards and scroll

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'letstaco-v7';
+var CACHE = 'letstaco-v8';
 var ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(cache) {
