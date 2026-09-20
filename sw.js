252612e92bddf4f7f11ad85cb6f26475b650853b
letstacodanang <letstacodanang@gmail.com>
2026-09-20 13:26:02 +0700

    Walk-up checkout screen (cash, bank QR, card) sending paid orders straight to the kitchen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'letstaco-v9';
+var CACHE = 'letstaco-v10';
 var ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(cache) {
